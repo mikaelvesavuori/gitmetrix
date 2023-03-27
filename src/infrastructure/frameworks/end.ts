4a7ccfdac4c6fc306c2a01f4322a9e30374f925f
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2023-03-27 16:45:01 +0200

    fix(): add content type to API responses

--- src/infrastructure/frameworks/end.ts
+++ src/infrastructure/frameworks/end.ts
@@ -3,13 +3,16 @@
  */
 export function end(statusCode = 201, message?: Record<string, any> | number | string) {
   if (!message) message = '';
+  const contentType =
+    typeof message === 'string' || typeof message === 'number' ? 'text/plain' : 'application/json';
 
   return {
     statusCode,
     body: JSON.stringify(message),
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': contentType
     }
   };
 }
